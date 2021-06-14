89458f0dfaa6a0d2214600c395bf965a5cc53eb3
unknown <aalshanbayev@gmail.com>
2021-06-13 23:38:14 -0400

    Navbar completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
 function App() {
   return (
-    <h2>Hi</h2>
+    <Navbar />
   );
 }
 
